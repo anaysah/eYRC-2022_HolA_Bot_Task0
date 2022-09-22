589b548346fd7afcb059ea5512e4638d800388e2
Kalind Karia <kalind1610@gmail.com>
2022-09-23 04:47:19 +0530

    fix: modified HB task0 exec, skeleton

--- eyrc-2022_holabot/scripts/task_0.py
+++ eyrc-2022_holabot/scripts/task_0.py
@@ -4,14 +4,14 @@
 *****************************************************************************************
 *
 *        		===============================================
-*           		HolA Bot (HB) Theme (eYRC 2022-23)
+*           		    HolA Bot (HB) Theme (eYRC 2022-23)
 *        		===============================================
 *
 *  This script should be used to implement Task 0 of HolA Bot (KB) Theme (eYRC 2022-23).
-*  
+*
 *  This software is made available on an "AS IS WHERE IS BASIS".
 *  Licensee/end user indemnifies and will keep e-Yantra indemnified from
-*  any and all claim(s) that emanate from the use of the Software or 
+*  any and all claim(s) that emanate from the use of the Software or
 *  breach of the terms of this agreement.
 *
 *****************************************************************************************
@@ -20,26 +20,16 @@
 # Team ID:			[ Team-ID ]
 # Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
 # Filename:			task_0.py
-# Functions:		
+# Functions:
 # 					[ Comma separated list of functions in this file ]
 # Nodes:		    Add your publishing and subscribing node
 
 
 ####################### IMPORT MODULES #######################
 import sys
+import traceback
 ##############################################################
 
-################# ADD GLOBAL VARIABLES HERE #################
-
-
-
-##############################################################
-
-################# ADD UTILITY FUNCTIONS HERE #################
-
-
-
-##############################################################
 
 def callback(data):
 	"""
@@ -57,12 +47,12 @@ def callback(data):
 	Returns:
 	---
         May vary depending on your logic.
-	
+
 	Example call:
 	---
         Depends on the usage of the function.
 	"""
-    
+
 
 def main():
 	"""
@@ -78,20 +68,35 @@ def main():
 	Returns:
 	---
         None
-	
+
 	Example call:
 	---
         main()
-	"""    
+	"""
 
-######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS PART #########    
+
+################# ADD GLOBAL VARIABLES HERE #################
+
+
+
+##############################################################
+
+
+################# ADD UTILITY FUNCTIONS HERE #################
+
+
+
+##############################################################
+
+
+######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS PART #########
 if __name__ == "__main__":
     try:
         print("------------------------------------------")
         print("         Python Script Started!!          ")
         print("------------------------------------------")
         main()
-        
+
     except:
         print("------------------------------------------")
         traceback.print_exc(file=sys.stdout)
